4ed3a8d8baa28fd8309552c0e8c5b8fa1ec3e481
Mehmet Yağcı <mehmetyagci53@gmail.com>
2023-01-03 23:51:11 +0300

    6. Task Method: ContinueWith compeleted

---
+++ TaskExamples/TaskConsoleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace TaskConsoleApp {
+    internal class Program {
+        //private async static Task Main(string[] args) {
+
+        //    Console.WriteLine("Hello World");
+
+        //    var mytask = new HttpClient().GetStringAsync("https://www.google.com").ContinueWith
+        //        (data => {
+        //            Console.WriteLine($"Uzunluk: {data.Result.Length}");
+        //        });
+
+        //    Console.WriteLine("Arada yapılacak işler");
+
+        //    await mytask;
+        //}
+
+
+        //private async static Task Main(string[] args) {
+
+        //    Console.WriteLine("Hello World");
+
+        //    var mytask = new HttpClient().GetStringAsync("https://www.google.com");
+
+        //    Console.WriteLine("Arada yapılacak işler");
+
+        //    var data = await mytask;
+        //    Console.WriteLine($"Uzunluk: {data.Length}");
+        //}
+        public static void calis(Task<string> data) {
+            // 100 satırlık bir kod 
+            Console.WriteLine($"Uzunluk: {data.Result.Length}");
+        }
+
+        private async static Task Main(string[] args) {
+
+            Console.WriteLine("Hello World");
+
+            var mytask = new HttpClient().GetStringAsync("https://www.google.com").ContinueWith(data);
+
+            Console.WriteLine("Arada yapılacak işler");
+
+            await mytask;
+        }
+
+
+    } // end of class
+} // end of namespace
