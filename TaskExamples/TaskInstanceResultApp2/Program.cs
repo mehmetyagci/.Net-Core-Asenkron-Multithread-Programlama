1e92c820c122ede9ad3b37ec0e53010e9ed8db38
Mehmet Yağcı <mehmetyagci53@gmail.com>
2023-01-08 00:51:20 +0300

    18. Task Instance Result Property-2

---
+++ TaskExamples/TaskInstanceResultApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TaskInstanceResultApp2 {
+    internal class Program {
+        private async static Task Main(string[] args) {
+
+            /// Yöntem 1
+            var task = new HttpClient().GetStringAsync("https://www.google.com/");
+
+            await task; // Thread bloklanmadan işlem yapıldı.
+
+            var result = task.Result; // Burada data elimizde artık. İstedğimiz gibi kullanabiliriz
+
+            Console.WriteLine($"result:{result.Substring(0, 100)}");
+
+            /// Yöntem 1
+            var task2 = new HttpClient().GetStringAsync("https://www.google.com/")
+                .ContinueWith((data) => {
+                    var result2 = data.Result;
+                    Console.WriteLine($"result2:{result2.Substring(0, 100)}");
+                });
+
+            await task2;
+        }
+
+        private static string GetData() {
+            return string.Empty;
+
+        }
+    }
+}
