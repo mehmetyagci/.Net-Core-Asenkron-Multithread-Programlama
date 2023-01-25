f60c85607b758b0ccb75c9fc1d56625b602f4acd
Mehmet Yağcı <mehmetyagci53@gmail.com>
2023-01-25 23:18:51 +0300

    35. Entity üzerinde PLINQ sorgu yazmak completed.

--- TaskExamples/4_34_AdventureWorksDatabaseFirst/Program.cs
+++ TaskExamples/4_34_AdventureWorksDatabaseFirst/Program.cs
@@ -12,14 +12,46 @@ using System.Threading.Tasks;
 
 namespace _4_34_AdventureWorksDatabaseFirst {
     internal class Program {
+
+        private static void WriteLog(Product p) {
+            Console.WriteLine($"ProductId:{p.ProductId} ProductName:{p.Name} ManagedThreadId:{Thread.CurrentThread.ManagedThreadId} log 'a kaydedildi");
+        }
+
         static void Main(string[] args) {
 
             AdventureWorksContext context = new AdventureWorksContext();
-
-            context.Product.Take(10).ToList().ForEach(x => {
-                Console.WriteLine(x.Name);
+            context.Product.AsParallel().ForAll(p => {
+                WriteLog(p);
             });
-
         }
     }
 }
+
+
+
+
+//context.Product.Take(10).ToList().ForEach(x => {
+//    Console.WriteLine(x.Name);
+//});
+
+// elimde var olan array üzerinde bir sorgu yapmak istiyorsak AsParallel()
+// context.Product tüm Product'ları alır. IQueryable
+//var product = (from p in context.Product.AsParallel()
+//               where p.ListPrice > 10M
+//               select p).Take(10);
+
+//var product2 = context.Product.AsParallel().Where(p => p.ListPrice > 10M).Take(10);
+//product.ForAll(x => {
+//    Console.WriteLine(x.Name);
+//});
+
+// IQueryable versiyon
+//IQueryable<Product> product = (from p in context.Product
+//                               where p.ListPrice > 10M
+//                               select p).Take(10);
+
+//var result =product.AsParallel().Where(x => x.Name.StartsWith('a')).For;
+
+//product.ToList().ForEach(p => {
+//    Console.WriteLine($"{p.Name} {p.ProductId} {p.ProductNumber}");
+//});
